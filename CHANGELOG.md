d23d95b25d34592b2c88c170ba378058966792f7
semantic-release <semantic-release>
2026-09-14 14:10:57 +0000

    chore: release v0.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 <!-- version list -->
 
+## v0.1.0 (2026-09-14)
+
+- Initial Release
+
 ## v0.21.1 (2026-09-11)
 
 ### Bug Fixes
--- src/fugleramme/__init__.py
+++ src/fugleramme/__init__.py
@@ -1,3 +1,3 @@
 """Fugleramme frame service: render the latest bird detection and serve stats."""
 
-__version__ = "0.21.1"
+__version__ = "0.1.0"
